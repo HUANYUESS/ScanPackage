9058331b450a7962066cc3f77a1f3f80b405e4e2
hycoke <hycoke@qq.com>
2023-04-20 16:32:34 +0800

    Init Project.

---
+++ src/test/java/com/sugoos/scanpackage/ScanPackageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sugoos.scanpackage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScanPackageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
